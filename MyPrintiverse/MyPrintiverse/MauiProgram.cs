a667ba2b8d79c2a502a9ce04361e07cc34e95f6c
Kacper Żabiński <kacperzab0@wp.pl>
2022-05-14 21:49:13 +0200

    Pull request fixes

--- MyPrintiverse/MyPrintiverse/MauiProgram.cs
+++ MyPrintiverse/MyPrintiverse/MauiProgram.cs
@@ -4,7 +4,7 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		var builder = MauiApp.CreateBuilder()
+		var appInstance = MauiApp.CreateBuilder()
 			.UseMauiApp<App>()
 			.ConfigureServices()
 			.ConfigureViews()
@@ -13,8 +13,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.Build();
 
-		return builder;
+		return appInstance;
 	}
 }
