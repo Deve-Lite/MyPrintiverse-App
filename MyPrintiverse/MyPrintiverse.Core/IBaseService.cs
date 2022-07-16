28b543f07c23a4911b9f3c7ff4c167235ea08216
Kacper Żabiński <kacperzab0@wp.pl>
2022-07-16 15:53:30 +0200

    cm

--- MyPrintiverse/MyPrintiverse.Core/IBaseService.cs
+++ MyPrintiverse/MyPrintiverse.Core/IBaseService.cs
@@ -2,25 +2,28 @@
 
 namespace MyPrintiverse.Core;
 
+/// <summary>
+/// Service base.
+/// </summary>
 public interface IBaseService
 {
 	/// <summary>
-	/// 
+	/// Message handler e.g alerts, prompts.
 	/// </summary>
 	public IMessageService MessageService { get; }
 	
 	/// <summary>
-	/// 
+	/// Logger handler
 	/// </summary>
 	public ILogger Logger { get; }
 	
 	/// <summary>
-	/// 
+	/// Config handler.
 	/// </summary>
 	public IConfigService<Config> ConfigService { get; }
 
 	/// <summary>
-	/// 
+	/// Session handler.
 	/// </summary>
 	public ISession Session { get; }
 }
